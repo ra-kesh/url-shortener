a2981a4d6a85fa34479706dc8b24dd63aae5e7cc
ra-kesh <iamrakeshkumar@pm.me>
2025-03-04 14:57:26 +0530

    added hosted turso db

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 import express from "express";
 import crypto from "crypto";
 import { PrismaClient } from "@prisma/client";
+import { createClient } from "@libsql/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+
+import "dotenv/config";
 
 const app = express();
 app.use(express.json());
 
-const prisma = new PrismaClient();
+const libsql = createClient({
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
+
+const adapter = new PrismaLibSQL(libsql);
+const prisma = new PrismaClient({ adapter });
 
 function generateShortCode() {
   return crypto.randomBytes(3).toString("hex");
